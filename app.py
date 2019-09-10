42b17a975f142a7462983ba3027d8dc2e9707914
satyam soni <satyamsoni@hotmail.co.uk>
2019-09-10 18:50:11 +0530

    added comments

--- app.py
+++ app.py
@@ -4,17 +4,21 @@ from flask_restful import Resource, Api
 from flask_cors import CORS
 from routes import ParseFeed
 
+# Initializing app and api
 app = Flask(__name__)
 api = Api(app=app)
+
+# for cross origin acceptance
 CORS(app)
 
-
+# Functional View for base URL
 @app.route('/')
 def home():
     return render_template('index.html')
 
 
+# Route for parse
 api.add_resource(ParseFeed, '/parse')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
